49813dae18605377aff41d62b9829c518715b54f
Yan <www324608yf@outlook.com>
2026-04-08 21:01:32 +0800

    turn to Rust

--- SuperSpaceNeo.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by EthanYan on 2026/4/5.
-//
-
-#include "SuperSpaceNeo.h"
-
-namespace SuperSpace
-{
-} // SuperSpace
--- src/main/main.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by EthanYan on 2026/4/5.
-//
-
-#include "main.h"
-
-namespace SuperSpaceNeo {
-} // SuperSpaceNeo
